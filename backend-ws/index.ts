0c74b0bbf99c3dce9e099dae0d0035be005bfcd2
alyonaVet <a.v.alenka2015@gmail.com>
2024-10-01 11:07:30 +0600

    added connectedClients array keep clients;

--- backend-ws/index.ts
+++ backend-ws/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import expressWs from "express-ws";
 import cors from "cors";
 import config from "./config";
+import {WebSocket} from "ws";
 
 const app = express();
 const port = 8000;
@@ -11,11 +12,23 @@ app.use(cors(config.corsOptions));
 
 const router = express.Router();
 
+const connectedClients: WebSocket[] = [];
+
 router.ws('/draw', (ws, req) => {
+    connectedClients.push(ws);
     console.log('Client connected');
+
+    ws.on('message', (message) => {
+        console.log('Received message:', message);
+        connectedClients.forEach(clientWs => {
+            clientWs.send(message);
+        });
+    });
 
     ws.on('close', () => {
         console.log('Client disconnected');
+        const index = connectedClients.indexOf(ws);
+        connectedClients.splice(index, 1);
     });
 });
 
